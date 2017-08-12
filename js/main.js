d82e0dadcd181a5900b42a609e86c3e68ea3895c
Viktor Koves <viktorkoves@gmail.com>
2017-08-12 00:02:27 -0500

    Added getSeason() and a console log that logs the current season

--- js/main.js
+++ js/main.js
@@ -1,4 +1,36 @@
 $(document).ready(function()
 {
-	console.log("Hello world!");
+	console.log("Current season: " + getSeason(new Date().getMonth() + 1));
 });
+
+// Get season based on a month number
+// Uses the typical calendar month numbers (1-12)
+// not JS months (0-11)
+function getSeason(month) {
+	season = '';
+
+	switch(month) {
+		case 12:
+		case 1:
+		case 2:
+			season = 'winter';
+			break;
+		case 3:
+		case 4:
+		case 5:
+			season = 'spring';
+			break;
+		case 6:
+		case 7:
+		case 8:
+			season = 'summer';
+			break;
+		case 9:
+		case 10:
+		case 11:
+			season = 'fall';
+			break;
+	}
+
+	return season;
+}
